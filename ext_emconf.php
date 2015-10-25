c287dd1384ea940e56ce461f6c7e594e15b6a636
Axel Böswetter <evilbmp@gmail.com>
2015-10-25 16:34:31 +0100

    * changes dep for TYPO3 version down to 7.5.0 to also support current TYPO3-Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.5.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
